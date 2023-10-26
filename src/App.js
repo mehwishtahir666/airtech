95b5162d1c097d861762c56e1068673f140ba1a6
harisalih <147698965+harisalih@users.noreply.github.com>
2023-10-26 12:11:17 +0500

    appServices

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import AppServices from './components/appServices/appServices';
 
 function App() {
   return (
     <>
-    
+      <BrowserRouter>
+        <Routes>
+          <Route path="/AppServices" element={ <AppServices/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
