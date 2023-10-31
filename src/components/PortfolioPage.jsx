a91b1a9fd798cc55a0fe2117a339152f4576888e
haiderHussnain <118504778+haiderHussnain@users.noreply.github.com>
2023-10-31 22:11:06 +0500

    forHaris

--- src/components/PortfolioPage.jsx
+++ src/components/PortfolioPage.jsx
@@ -57,6 +57,97 @@ const Projects = () => {
           </Carousel>
         </div>
       </div>
+
+{/*  second */}
+
+<div className='projects'>
+        <div className='heading-box'>
+          <h1>Mobiile Applications Projects</h1>
+          <p>Our team build real-world projects to put theories into the real world implimentaion.</p>
+        </div>
+        <div className='button-box'>
+          <button>Social Media apps</button>
+          <button>Educational apps</button>
+          <button>Utility apps</button>
+          <button>Lifestyle apps</button>
+          <button>Entertainment apps</button>
+          <button>Food apps</button>
+        </div>
+
+        {/* ================================= */}
+
+
+        <div className='card-box'>
+
+          <Carousel breakPoints={breakPoints} >
+            {items.map((item) => (
+
+              <div className="card-1">
+                <img src="/images/hloo3.jpg" alt="" />
+                <h3>Project Name</h3>
+                <small>HTML CSS</small>
+                <p>E-Commerce</p>
+                <p> services, and information to users worldwide, catering to various needs and interests.</p>
+                <p className="icons">
+                <AiFillStar className="clr-yellow"/>
+                <AiFillStar className="icons clr-yellow"/>
+                <AiFillStar className="icons clr-yellow"/>
+                <AiOutlineStar className="icons"/>
+                <AiOutlineStar className="icons"/>
+                </p>
+                <div className='button-div'>
+                  <p className="button-p">VISIT WEBSITE</p>
+                </div>
+              </div>
+            ))}
+          </Carousel>
+        </div>
+      </div>
+
+{/* ----third --------- */}
+
+<div className='projects'>
+        <div className='heading-box'>
+          <h1>SEO</h1>
+          <p>Our team build real-world projects to put theories into the real world implimentaion.</p>
+        </div>
+        <div className='button-box'>
+          <button>Keywords</button>
+          <button>SERPs</button>
+          <button>title and hooks</button>
+          <button>main & sub headings</button>
+        </div>
+
+        {/* ================================= */}
+
+
+        <div className='card-box'>
+
+          <Carousel breakPoints={breakPoints} >
+            {items.map((item) => (
+
+              <div className="card-1">
+                <img src="/images/hloo3.jpg" alt="" />
+                <h3>Project Name</h3>
+                <small>HTML CSS</small>
+                <p>E-Commerce</p>
+                <p> services, and information to users worldwide, catering to various needs and interests.</p>
+                <p className="icons">
+                <AiFillStar className="clr-yellow"/>
+                <AiFillStar className="icons clr-yellow"/>
+                <AiFillStar className="icons clr-yellow"/>
+                <AiOutlineStar className="icons"/>
+                <AiOutlineStar className="icons"/>
+                </p>
+                <div className='button-div'>
+                  <p className="button-p">VISIT WEBSITE</p>
+                </div>
+              </div>
+            ))}
+          </Carousel>
+        </div>
+      </div>
+
     </>
   )
 }
---
+++ src/components/ServicesNew.jsx
@@ -0,0 +1,45 @@
+import React,{useState} from 'react'
+
+import { BsBagCheck } from "react-icons/bs";
+import Carousel from "react-elastic-carousel";
+
+const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 2, itemsToScroll: 2, pagination: false },
+    { width: 850, itemsToShow: 3 },
+    { width: 1150, itemsToShow: 4, itemsToScroll: 2 },
+    { width: 1450, itemsToShow: 5 },
+    { width: 1750, itemsToShow: 6 },
+];
+
+const Realstate = () => {
+    const [items, setItems] = useState([1, 2, 3, 4, 5, 6,7]);
+    return (
+        <div className='realstate'>
+            <div className="left">
+                <h1>Our <span>Real Estate</span></h1>
+                <h1>Digital Marketing</h1>
+                <h1>Services</h1>
+                <p >At BrandCurb, we offer a comprehensive suite of digital marketing services tailored specifically to the needs of real estate businesses. From search engine optimization to social media marketing, we have the expertise and experience to help you succeed online.</p>
+                <button>Book a free Consultation</button>
+            </div>
+            <div className="right">
+                {/* <div > */}
+                    <Carousel id='crouseee' className='crousee' breakPoints={breakPoints} >
+                        {items.map((item) => (
+                            <div className="padd">
+                              <div  className="box">
+                              <h3>Website Design &</h3>
+                              <h3>Development</h3>
+                              <p className='p-1'>Website Design & Development</p>
+                              <img src="/images/img7.webp" alt="" />
+                              <li><p> <span>from</span> $480 month</p> <BsBagCheck className='icon001'/>  </li>
+                            </div>
+                          </div>
+                        ))}
+                    </Carousel>
+            </div>
+        </div>
+    )
+}
+export default Realstate
