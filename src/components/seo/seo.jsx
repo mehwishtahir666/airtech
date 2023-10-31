9b661b4bea24e618fd8eb8fe8d489899c9f3b864
MariumShahid18 <145197996+MariumShahid18@users.noreply.github.com>
2023-10-31 14:59:29 +0500

    seo

---
+++ src/components/seo/seo.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./seo.css"
+const Seo=()=>{
+    return(
+        <>
+<div className="container-fluid border border-primary h-100 ">
+<button type="button" class="btn btn-primary">Primary</button>
+</div>
+        </>
+    )
+}
+export default Seo
